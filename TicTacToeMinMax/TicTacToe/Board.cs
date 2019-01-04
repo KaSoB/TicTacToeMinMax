f191b130725118750f8e62b0969c07e06bf7ed59
KamilSobien <kamil.sobien@gmail.com>
2019-01-04 13:43:25 +0100

    added Board, Game, MinMax function

---
+++ TicTacToeMinMax/TicTacToe/Board.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace TicTacToeMinMax.TicTacToe {
+    public class Board {
+        // note: Blank = 0 by default 
+        public enum State { Blank, X, O };
+
+        public State[] BoardArray { get; private set; } = new State[9];
+
+        public void Pick(int position, State state) {
+            BoardArray[position] = state;
+        }
+
+        public bool IsWinner(State state) {
+            if ((BoardArray[0] == state && BoardArray[1] == state && BoardArray[2] == state) ||
+                (BoardArray[3] == state && BoardArray[4] == state && BoardArray[5] == state) ||
+                (BoardArray[6] == state && BoardArray[7] == state && BoardArray[8] == state) ||
+                (BoardArray[0] == state && BoardArray[3] == state && BoardArray[6] == state) ||
+                (BoardArray[1] == state && BoardArray[4] == state && BoardArray[7] == state) ||
+                (BoardArray[2] == state && BoardArray[5] == state && BoardArray[8] == state) ||
+                (BoardArray[0] == state && BoardArray[4] == state && BoardArray[8] == state) ||
+                (BoardArray[2] == state && BoardArray[4] == state && BoardArray[6] == state)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public int[] GetPositionsOf(State state) {
+            return BoardArray
+                .Select((f, i) => new { f, i })
+                .Where(x => x.f == state)
+                .Select(x => x.i)
+                .ToArray();
+        }
+        public State this[int key] {
+            get {
+                return BoardArray[key];
+            }
+            set {
+                BoardArray[key] = value;
+            }
+        }
+        public void Display() {
+            for (int i = 0 ; i < 3 ; i++) {
+                for (int j = 0 ; j < 3 ; j++) {
+                    var state = BoardArray[i * 3 + j];
+                    var eChar = state == State.Blank ? "-" : state.ToString();
+                    Console.Write($" {eChar} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ TicTacToeMinMax/TicTacToe/Game.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static TicTacToeMinMax.TicTacToe.Action;
+using static TicTacToeMinMax.TicTacToe.Board;
+
+namespace TicTacToeMinMax.TicTacToe {
+    public class Game {
+        static readonly Random random = new Random();
+        readonly Board board = new Board();
+
+        readonly Player player1 = new Player(State.O);
+        readonly Player player2 = new Player(State.X);
+        private Player currentPlayer;
+
+        public void Play() {
+            currentPlayer = player1;
+            while (!IsEndGame()) {
+                Console.WriteLine("Now move: " + currentPlayer.State);
+                int index = 0;
+
+                if (currentPlayer == player1) {
+                    index = MinMaxFunction(currentPlayer).Index;
+                } else {
+                    // be carefull, you might override player1's move
+                    index = int.Parse(Console.ReadLine());
+                }
+
+
+                board[index] = currentPlayer.State;
+                board.Display();
+                ChangePlayer();
+            }
+            EndGameMessage();
+        }
+
+        public Action MinMaxFunction(Player player) {
+            var blankPositions = board.GetPositionsOf(State.Blank);
+
+            // check winner
+            if (board.IsWinner(player1.State)) {
+                return new Action() { Result = Score.Max };
+            } else if (board.IsWinner(player2.State)) {
+                return new Action() { Result = Score.Min };
+            } else if (!blankPositions.Any()) {
+                return new Action() { Result = Score.Zero };
+            }
+            // for each posible move, calculate score
+            List<Action> moves = new List<Action>();
+            for (int i = 0 ; i < blankPositions.Length ; i++) {
+                Action move = new Action {
+                    Index = blankPositions[i]
+                };
+
+                // set temporary
+                board.BoardArray[blankPositions[i]] = player.State;
+
+                if (player == player1) {
+                    move.Result = MinMaxFunction(player2).Result;
+                } else if (player == player2) {
+                    move.Result = MinMaxFunction(player1).Result;
+                }
+
+                // reset
+                board.BoardArray[blankPositions[i]] = State.Blank;
+
+                moves.Add(move);
+            }
+
+
+            var possibleMoves = moves.Where(it => it.Result == ((player == player1) ? Score.Max : Score.Min)).ToList();
+            if (!possibleMoves.Any()) {
+                possibleMoves = moves.Where(it => it.Result == Score.Zero).ToList();
+            }
+            if (!possibleMoves.Any()) {
+                possibleMoves = moves;
+            }
+            int randomIndex = random.Next(0, possibleMoves.Count());
+
+            Action bestMove = possibleMoves[randomIndex];
+            return bestMove;
+        }
+
+        void ChangePlayer() {
+            currentPlayer = (currentPlayer == player1) ? player2 : player1;
+        }
+
+        bool IsEndGame() {
+            return board.IsWinner(player1.State) || board.IsWinner(player2.State) || !board.GetPositionsOf(State.Blank).Any();
+        }
+        void EndGameMessage() {
+            if (board.IsWinner(player1.State)) {
+                Console.WriteLine("Player1 won!");
+            } else if (board.IsWinner(player2.State)) {
+                Console.WriteLine("Player2 won!");
+            } else if (!board.GetPositionsOf(State.Blank).Any()) {
+                Console.WriteLine("Tie!");
+            }
+        }
+    }
+}
